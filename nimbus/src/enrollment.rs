799917dd96c80a23fc4bd9dde25b831bc00abcbf
James Hugman <james@hugman.tv>
2021-03-09 17:39:16 +0000

    Use import aliases to change name to NimbusError

--- nimbus/src/enrollment.rs
+++ nimbus/src/enrollment.rs
@@ -2,7 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 use crate::persistence::{Database, StoreId, Writer};
-use crate::{error::Result, Error};
+use crate::{error::Result, NimbusError as Error};
 use crate::{evaluator::evaluate_enrollment, persistence::Readable};
 use crate::{AppContext, AvailableRandomizationUnits, EnrolledExperiment, Experiment};
 
